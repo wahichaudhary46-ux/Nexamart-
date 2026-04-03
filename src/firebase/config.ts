6c816092888df8fd0b9783ddabe4f5786ae28c07
Wahi Chaudhary <wahichaudhary31@gmail.com>
2026-04-03 09:58:10 +0000

    "I have generated new Firebase API keys for my project. Please find the

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,12 +4,11 @@
  * These keys are essential for connecting the client-side SDK to Firebase services.
  */
 export const firebaseConfig = {
-  apiKey: "AIzaSyBo9AdImafpscXrIcdP22HdN8um4xPaxOQ",
-  authDomain: "nexamart-36ac2.firebaseapp.com",
-  databaseURL: "https://nexamart-36ac2-default-rtdb.firebaseio.com",
-  projectId: "nexamart-36ac2",
-  storageBucket: "nexamart-36ac2.firebasestorage.app",
-  messagingSenderId: "769225832167",
-  appId: "1:769225832167:web:c49e11d631afb6a6a55442",
-  measurementId: "G-ZFHMK80G24"
+  apiKey: "AIzaSyCZ6-iJTcaYNOomJmpTFMMZmtf5kvoNW40",
+  authDomain: "nexamart-c462f.firebaseapp.com",
+  projectId: "nexamart-c462f",
+  storageBucket: "nexamart-c462f.firebasestorage.app",
+  messagingSenderId: "212119658915",
+  appId: "1:212119658915:web:2a9bec5c633fa4f6b73a16",
+  measurementId: "G-CSQ55H0KEK"
 };
